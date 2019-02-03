090d8ee86704a0aa920d5be37b18bc37f0e5226b
eddozeynal <Eddo590774>
2019-02-04 00:22:00 +0400

    Test 1

--- MobileGridWindowsFormsTest/Program.cs
+++ MobileGridWindowsFormsTest/Program.cs
@@ -17,6 +17,7 @@ namespace MobileGridWindowsFormsTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
